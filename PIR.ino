283167140fd1834dd9d0c4fa0308c4aa5232b221
LeeSoomi <70505715+LeeSoomi@users.noreply.github.com>
2021-08-10 22:57:52 +0900

    Update PIR.ino

--- PIR.ino
+++ PIR.ino
@@ -10,14 +10,12 @@ void setup() {
 void loop( ) {
   if (digitalRead(PIR) == HIGH) {        
     Serial.println("SENSOR ON");     
-    for(int i=1 ; i<5; i++){
     digitalWrite(Led , HIGH );
-    delay(300); 
-    digitalWrite(Led , LOW );
-    delay(200);      
-    }        
-  }  else {                                  
-    Serial.println("  ");                   
+    delay(200);         
+  }  
+  else {                                  
+    Serial.println("SENSOR OFF"); 
+    digitalWrite(Led , LOW );                  
   }
   delay(100);                             
 }
