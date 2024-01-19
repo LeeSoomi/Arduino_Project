f1fdcc1ec1f437c2c4044ba76c7a2c04c3e620aa
LeeSoomi <70505715+LeeSoomi@users.noreply.github.com>
2024-01-19 18:49:26 +0900

    Update OLED_i2c_ultra.ino

--- OLED_i2c_ultra.ino
+++ OLED_i2c_ultra.ino
@@ -1,1 +1,58 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define trigPin 13                 
+#define echoPin 12  
+
+
+//#define LOGO_HEIGHT   16
+//#define LOGO_WIDTH    16
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+
+}
+
+void loop() {
+    digitalWrite(trigPin, LOW);
+    digitalWrite(echoPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+
+  // echoPin 이 HIGH를 유지한 시간을 저장 한다.
+    unsigned long duration = pulseIn(echoPin, HIGH); 
+  // HIGH 였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산 한다.
+    float distance = ((float)(340 * duration) / 10000) / 2;  
+  Serial.print(distance);
+  Serial.println("cm");
+  
+  display.setTextSize(3);             
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(1,1);
+  display.println(distance);
+  display.display();
+}
