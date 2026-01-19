e291a63161a23e6ef535ae3aae8d714c2dd1dda8
LeeSoomi <70505715+LeeSoomi@users.noreply.github.com>
2026-01-19 15:10:44 +0900

    Update Huskey_Neopixel.ino

--- Huskey_Neopixel.ino
+++ Huskey_Neopixel.ino
@@ -9,23 +9,22 @@
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-long interval_red = 10000;
-long interval_green = 15000;
-long previousMillis = 0;
+unsigned long interval_red = 10000;
+unsigned long interval_green = 15000;
+unsigned long previousMillis = 0;
 
 HUSKYLENS huskylens;
-void printResult(HUSKYLENSResult result);
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-    while (!huskylens.begin(Wire))
-    {
-        Serial.println(F("Begin failed!"));
-        Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
-        Serial.println(F("2.Please recheck the connection."));
-        delay(100);
-    }
+
+  while (!huskylens.begin(Wire)) {
+    Serial.println(F("Begin failed!"));
+    Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
+    Serial.println(F("2.Please recheck the connection."));
+    delay(100);
+  }
 
   pixels.begin();
   pixels.clear();
@@ -33,48 +32,64 @@ void setup() {
   delay(1000);
 }
 
-void loop() {
-  pixels.clear();
-  signalLamp();
-  if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
-  else
-    {
-      while (huskylens.available())
-      {
-        HUSKYLENSResult result = huskylens.read();
-        if (result.command == COMMAND_RETURN_BLOCK){
-          if (result.ID == 1){
-            Serial.println("Detect Person!");
-            pixels.setPixelColor(0, pixels.Color(150, 150, 0));
-            pixels.show();
-          }
-          else{
-            Serial.println("No Person!");
-            pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-            pixels.show();
-          }
-        }
-      }    
+bool detectPersonID1() {
+  bool detected = false;
+
+  if (!huskylens.request()) {
+    Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+    return false;
+  }
+
+  while (huskylens.available()) {
+    HUSKYLENSResult result = huskylens.read();
+
+    if (result.command == COMMAND_RETURN_BLOCK) {
+      if (result.ID == 1) {
+        detected = true;
+      }
     }
+  }
+
+  return detected;
 }
 
-void signalLamp(){
+void showYellowOnly() {
+  pixels.clear();
+  pixels.setPixelColor(0, pixels.Color(150, 150, 0)); // 노란색
+  pixels.setPixelColor(1, pixels.Color(0, 0, 0));     // 초록 끔
+  pixels.setPixelColor(2, pixels.Color(0, 0, 0));     // 빨강 끔
+  pixels.show();
+}
+
+void showSignalLamp() {
   unsigned long currentMillis = millis();
+  unsigned long elapsed = currentMillis - previousMillis;
+
   Serial.println(currentMillis);
 
-  if( currentMillis - previousMillis <= interval_red ) {
-    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-    pixels.setPixelColor(1, pixels.Color(0, 0, 0));
-    pixels.setPixelColor(2, pixels.Color(150, 0, 0));
+  if (elapsed <= interval_red) {
+    pixels.clear();
+    pixels.setPixelColor(2, pixels.Color(150, 0, 0)); // 빨강
     pixels.show();
   }
-  else if( currentMillis - previousMillis > interval_red && currentMillis - previousMillis <= interval_green ) {
-    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-    pixels.setPixelColor(1, pixels.Color(0, 150, 0));
-    pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+  else if (elapsed <= (interval_red + interval_green)) {
+    pixels.clear();
+    pixels.setPixelColor(1, pixels.Color(0, 150, 0)); // 초록
     pixels.show();
   }
-  else if (currentMillis - previousMillis > interval_green){
+  else {
     previousMillis = currentMillis;
+  }
+}
+
+void loop() {
+  bool personDetected = detectPersonID1();
+
+  if (personDetected) {
+    Serial.println("Detect Person!");
+    showYellowOnly();
+  } else {
+    Serial.println("No Person!");
+    showSignalLamp();
   }
 }
